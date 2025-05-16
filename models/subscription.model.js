2d61f93a952d3197969c7af934cf7c0f1cba3fd1
Usama Ayoub <usamabinayoub@gmail.com>
2025-05-16 16:08:56 +0500

    subscription schema added

---
+++ models/subscription.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLength:2,
+        maxLength: 100,
+    },
+    price:{
+        type: String,
+        require: [true, 'Subscription price are required'],
+        min: [0, 'Price must be greater than 0']
+    },
+    currency:{
+        type: String,
+        enum: ['USD', 'EUR', 'GBP'],
+        default: 'USD'
+    },
+    frequency:{
+        type: String,
+        enum: ['daily','weekly','monthly','yearly']
+    },
+    category:{
+        type: String,
+        enum: ['sports','news','entertainment','lifestyle','technology','finance','politics','others'],
+        required: true
+    },
+    paymentMethod:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    status:{
+        type: String,
+        enum: ['active','cancelled','expired'],
+        default: 'active'
+    },
+    startDate:{
+        type: Date,
+        required: true,
+        validate:{
+            validator: (value)=> value <= new Date(),
+            message: 'Start date must be in past'
+        }
+    },
+    renewaltDate:{
+        type: Date,
+        required: true,
+        validate:{
+            validator: function(value){
+                return value > this.startDate;
+            },
+            message: 'Start date must be after the start date'
+        }
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId(),
+        ref: 'User',
+        required: true,
+        index: true,
+    }
+
+}, {timestamps: true})
+
+subscriptionSchema.pre('save', function(){
+    
+})
